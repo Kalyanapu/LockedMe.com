eaef7230a529e93a626f1f7e8d6f9adfd2e709aa
Naveen kumar <kalyanapunaveenkumar1996@gmail.com>
2021-08-09 19:36:59 +0530

    Retriving the filenames from the user

---
+++ src/com/lockedme/LockedMeMain.java
@@ -0,0 +1,13 @@
+package com.lockedme;
+
+public class LockedMeMain {
+	
+	// Here We are making static final so that it will be a constant user cannot
+	// change the path
+	static final String folderPath = "D:\\Phase-01 Project\\LockedMeProjectFiles";
+
+	public static void main(String[] args) {
+
+	}
+
+}

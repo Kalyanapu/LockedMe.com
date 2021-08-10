f4023140df2c48851f3583c0342ad8e4a9955c8f
Naveen kumar <kalyanapunaveenkumar1996@gmail.com>
2021-08-10 14:46:05 +0530

    getAllFileNames is added

--- src/com/lockedme/FileOperations.java
+++ src/com/lockedme/FileOperations.java
@@ -7,12 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileOperations {
-	/**
-	 * This method will return the file names from the folder
-	 * 
-	 * @param folderPath
-	 * @return List<String>
-	 */
+
 	public static List<String> getAllFiles(String folderPath) {
 		// Creating the file Object
 		File file = new File(folderPath);
@@ -31,45 +26,4 @@ public class FileOperations {
 
 	}
 
-	/**
-	 * 
-	 * @param folderPath
-	 * @param fileName
-	 * @param content
-	 * @return
-	 */
-
-	public static boolean writeContentToFile(String folderPath, String fileName, List<String> content) {
-
-		File f = new File(folderPath, fileName);
-		try {
-			FileWriter fw = new FileWriter(f);
-
-			for (String s : content) {
-				fw.write(s + "\n");
-			}
-			fw.flush();
-			fw.close();
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-
-		}
-
-	}
-
-	public static boolean deleteFile(String folderPath, String fileName) {
-
-		File file = new File(folderPath + "\\" + fileName);
-		try {
-			if (file.delete())
-				return true;
-			else
-				return false;
-
-		} catch (Exception e) {
-			return false;
-		}
-	}
 }
--- src/com/lockedme/LockedMeMain.java
+++ src/com/lockedme/LockedMeMain.java
@@ -1,56 +1,17 @@
 package com.lockedme;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class LockedMeMain {
 
-	// Here We are making static final so that it will be a constant user cannot
-	// change the path
 	static final String folderPath = "D:\\java-01\\Phase-01 Project\\LockedMeProjectFiles";
 
 	public static void main(String[] args) {
+
 		List<String> fileNames = FileOperations.getAllFiles(folderPath);
 		for (String f : fileNames) {
 			System.out.println(f);
 		}
 
-		Scanner sc = new Scanner(System.in);
-		String fileName;
-		int linesCount;
-		List<String> content = new ArrayList<String>();
-		System.out.println("Enter the filename");
-		fileName = sc.nextLine();
-
-		System.out.println("Enter the how many lines in the file:");
-		linesCount = Integer.parseInt(sc.nextLine());
-
-		for (int i = 1; i <= linesCount; i++) {
-			System.out.println("Enter line" + i + ":");
-			content.add(sc.nextLine());
-		}
-
-		boolean isSaved = FileOperations.writeContentToFile(folderPath, fileName, content);
-		if (isSaved) {
-			System.out.println("File and data saved successfully");
-		} else {
-			System.out.println("Some error occurred.Please contact development team");
-		}
-
-		String f;
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the file name to be deleted:");
-		f = scanner.nextLine();
-
-		boolean isDeleted = FileOperations.deleteFile(folderPath, fileName);
-		if (isDeleted)
-			System.out.println("file deleted successfully");
-		else
-			System.out.println("Either file is no there  or some access failure");
-
 	}
-	
-
 }
